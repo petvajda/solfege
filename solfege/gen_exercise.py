5adbeb312e9a3bdbfb3ef778c692d9bd206f2790
petvajda <petvajda@gmail.com>
2017-11-09 09:58:27 +0100

    Separate scale generation exercise from main code

---
+++ solfege/gen_exercise.py
@@ -0,0 +1,47 @@
+import random
+
+#############
+### NOTES ###
+#############
+NOTES_NORMAL = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+NOTES_SHARP  = list(map(lambda x: x+'#', NOTES_NORMAL))
+NOTES_FLAT   = list(map(lambda x: x+'b', NOTES_NORMAL))
+ALL_NOTES    = NOTES_NORMAL + NOTES_SHARP + NOTES_FLAT
+
+######################
+### PENTATON MODES ###
+######################
+PENTATON = [1, 2, 3, 5, 6]
+
+####################
+### SCALES MODES ###
+####################
+MODES = [1, 2, 3, 4, 5, 6, 7]
+
+######################
+### NAMES OF MODES ###
+######################
+
+def check_order(list):
+    for i in range(len(list)-1):
+        if abs(list[i] - list[i+1]) <= 2:
+            return True
+    return False
+
+def rand_mode(modes):
+    while check_order(modes):    
+        a = random.randrange(len(modes)-1)
+        modes[a], modes[a+1] = modes[a+1], modes[a]
+        # print('{0} {1} {2}'.format(a, a+1, modes))
+    return modes
+
+def exercise(modes):
+    print(ALL_NOTES[random.randrange(len(ALL_NOTES))], end=' ')
+    print(rand_mode(modes))
+
+def gen_exercise():
+    exercise(PENTATON)
+    exercise(MODES)
+
+if __name__ == '__main__':
+    gen_exercise()
--- solfege/solfege.py
+++ solfege/solfege.py
@@ -1,26 +1,5 @@
-import random
 from numpy import roll
-#############
-### NOTES ###
-#############
-NOTES_NORMAL = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
-NOTES_SHARP  = list(map(lambda x: x+'#', NOTES_NORMAL))
-NOTES_FLAT   = list(map(lambda x: x+'b', NOTES_NORMAL))
-ALL_NOTES    = NOTES_NORMAL + NOTES_SHARP + NOTES_FLAT
 
-######################
-### PENTATON MODES ###
-######################
-PENTATON = [1, 2, 3, 5, 6]
-
-####################
-### SCALES MODES ###
-####################
-MODES = [1, 2, 3, 4, 5, 6, 7]
-
-######################
-### NAMES OF MODES ###
-######################
 NAMES_MODES = {}
 NAMES_MODES["I"]   = "Ionian"
 NAMES_MODES["II"]  = "Dorian"
@@ -29,23 +8,6 @@ NAMES_MODES["IV"]  = "Lydian"
 NAMES_MODES["V"]   = "Mixolydian"
 NAMES_MODES["VI"]  = "Aeolian"
 NAMES_MODES["VII"] = "Locrian"
-
-def check_order(list):
-    for i in range(len(list)-1):
-        if abs(list[i] - list[i+1]) <= 2:
-            return True
-    return False
-
-def rand_mode(modes):
-    while check_order(modes):    
-        a = random.randrange(len(modes)-1)
-        modes[a], modes[a+1] = modes[a+1], modes[a]
-        # print('{0} {1} {2}'.format(a, a+1, modes))
-    return modes
-
-def exercise(modes):
-    print(ALL_NOTES[random.randrange(len(ALL_NOTES))], end=' ')
-    print(rand_mode(modes))
 
 class Note:    
     __reference_sharps  = [ "C", "C#", "D", "D#", "E", "F", "F#",
@@ -90,10 +52,4 @@ class Scale:
 
     def __str__(self):
         return " ".join(str(n) for n in self.notes)
-        
-def test_exercise():
-    exercise(PENTATON)
-    exercise(MODES)
     
-if __name__ == '__main__':
-    test_exercise()
