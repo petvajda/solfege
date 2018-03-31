dca40c92dbc9d8732f389a6fb0cd4bfe7b5badc3
Peter Vajda <petvajda@gmail.com>
2018-03-31 18:00:33 +0200

    Improved whole and half steps functions

--- solfege/solfege.py
+++ solfege/solfege.py
@@ -13,12 +13,16 @@ class Note:
     __chromatic_sharps  = [ "C", "C#", "D", "D#", "E", "F", "F#",
                             "G", "G#", "A", "A#", "B"]
     __chromatic_flats   = [ "C",  "Db", "D",  "Eb", "E",  "F",
-                            "Gb", "G",  "Ab", "A",  "Bb", "B"]
+                            "Gb", "G",  "Ab", "A",  "Bb", "Cb"]
     
     __base_notes        = ["C", "D", "E", "F", "G", "A", "B"]
     
     def __init__(self, note, use_sharp=True):
         self.use_sharp = use_sharp
+        if note[1:] == "#":
+            self.use_sharp = True
+        elif note[1:] == "b":
+            self.use_sharp = False
         note = note.capitalize()
         if note in self.__chromatic_sharps or self.__chromatic_flats:
             self.note  = note
@@ -38,33 +42,21 @@ class Note:
         i = self.__chromatic.index(self.note)
         return Note(self.__chromatic[(i - a) % len(self.__chromatic)], self.use_sharp)
 
-#    TODO: Make it simpler
-#    def w_up(self):
-#        return Note(self.__chromatic[self.__chromatic.index(self.note) + 2])
-        
     def w_up(self):
-        end = ""
-        mod = 1
-        i = self.__base_notes.index(self.note[:1])
-        if self.note[:1] == "E" or self.note[:1] == "B":
-            if self.note[1:] == "b":
-                mod = mod - 1
-            else:
-                end = "#"
-        if self.note[1:] == "#":
-            end = "#"
-        elif self.note[1:] == "b":
-            end = "b"
-        return self.__base_notes[(i + 1) % len(self.__base_notes)] + end * mod
-
+        return Note(self.__chromatic[(self.__chromatic.index(self.note) + 2)
+            % len(self.__chromatic)])
+        
     def h_up(self):
-        return Note(self.__chromatic[self.__chromatic.index(self.note) + 1])
+        return Note(self.__chromatic[(self.__chromatic.index(self.note) + 1)
+            % len(self.__chromatic)])
 
     def w_down(self):
-        pass
+        return Note(self.__chromatic[(self.__chromatic.index(self.note) - 2)
+            % len(self.__chromatic)])
 
     def h_down(self):
-        return Note(self.__chromatic[self.__chromatic.index(self.note) - 1])
+        return Note(self.__chromatic[(self.__chromatic.index(self.note) - 1)
+            % len(self.__chromatic)])
 
 class Scale:
     __base_notes            = ["C", "D", "E", "F", "G", "A", "B"]
--- tests/tests_notes.py
+++ tests/tests_notes.py
@@ -18,14 +18,14 @@ class TestNotes(unittest.TestCase):
 
     def test_add_notes_flat(self):
         self.assertEqual(str(Note("C", False) + 1), "Db")
-        self.assertEqual(str(Note("C", False) + 2), "D")
+        self.assertEqual(str(Note("C"       ) + 2), "D")
         self.assertEqual(str(Note("C", False) + 3), "Eb")
-        self.assertEqual(str(Note("C", False) + 4), "E")
-        self.assertEqual(str(Note("C", False) + 5), "F")
+        self.assertEqual(str(Note("C",      ) + 4), "E")
+        self.assertEqual(str(Note("C",      ) + 5), "F")
         self.assertEqual(str(Note("C", False) + 6), "Gb")
-        self.assertEqual(str(Note("C", False) + 7), "G")
-        self.assertEqual(str(Note("C", False) + 11), "B")
-        self.assertEqual(str(Note("C", False) + 12), "C")
+        self.assertEqual(str(Note("C",      ) + 7), "G")
+        self.assertEqual(str(Note("C",      ) + 11), "B")
+        self.assertEqual(str(Note("C",      ) + 12), "C")
         
     def test_sub_notes(self):
         self.assertEqual(str(Note("D") - 1), "C#")
@@ -41,33 +41,38 @@ class TestNotes(unittest.TestCase):
         self.assertEqual(str(Note("E") - 9), "G")    
 
     def test_whole_up(self):
-        self.assertEqual(str(Note("C" ).w_up()),  "D")
-        self.assertEqual(str(Note("C#").w_up()),  "D#")
-        self.assertEqual(str(Note("D" ).w_up()),  "E")
-        self.assertEqual(str(Note("D#").w_up()),  "E#")
-#       self.assertEqual(str(Note("D#").w_up()),  "F")
-        self.assertEqual(str(Note("E" ).w_up()),  "F#")
-        self.assertEqual(str(Note("B" ).w_up()),  "C#")
-        self.assertEqual(str(Note("Cb").w_up()),  "Db")
-        self.assertEqual(str(Note("Eb").w_up()),  "F")
-        self.assertEqual(str(Note("Bb").w_up()),  "C")
-        self.assertEqual(str(Note("B" ).w_up()),  "C#")
-        
+        self.assertEqual(str(Note("C"        ).w_up()), "D")
+        self.assertEqual(str(Note("C#"       ).w_up()), "D#")
+        self.assertEqual(str(Note("D"        ).w_up()), "E")
+        self.assertEqual(str(Note("D#"       ).w_up()), "F")
+        self.assertEqual(str(Note("E"        ).w_up()), "F#")
+        self.assertEqual(str(Note("B"        ).w_up()), "C#")
+        self.assertEqual(str(Note("Cb", False).w_up()), "Db")
+        self.assertEqual(str(Note("Eb", False).w_up()), "F")
+        self.assertEqual(str(Note("Bb", False).w_up()), "C")
+        self.assertEqual(str(Note("B"        ).w_up()), "C#")
+
+    def test_whole_down(self):
+        self.assertEqual(str(Note("C",       ).w_down()), "A#")
+        self.assertEqual(str(Note("D"        ).w_down()), "C")
+        self.assertEqual(str(Note("Db", False).w_down()), "Cb")
+        self.assertEqual(str(Note("F",  False).w_down()), "Eb")
+
     def test_half_up(self):        
-        self.assertEqual(str(Note("C"  ).h_up()), "C#")
-        self.assertEqual(str(Note("C#" ).h_up()), "D")
-        self.assertEqual(str(Note("D#" ).h_up()), "E")
-        self.assertEqual(str(Note("G"  ).h_up()), "G#")
-#       TODO  
-#       self.assertEqual(str(Note("Cb" ).h_up()), "C")
-#       self.assertEqual(str(Note("Db" ).h_up()), "D")
-#       self.assertEqual(str(Note("Eb" ).h_up()), "E")
+        self.assertEqual(str(Note("C"        ).h_up()), "C#")
+        self.assertEqual(str(Note("C#"       ).h_up()), "D")
+        self.assertEqual(str(Note("D#"       ).h_up()), "E")
+        self.assertEqual(str(Note("G"        ).h_up()), "G#") 
+        self.assertEqual(str(Note("Cb", False).h_up()), "C")
+        self.assertEqual(str(Note("Db", False).h_up()), "D")
+        self.assertEqual(str(Note("Eb", False).h_up()), "E")
                 
     def test_half_down(self):        
-        self.assertEqual(str(Note("C#" ).h_down()), "C")
-        self.assertEqual(str(Note("D#" ).h_down()), "D")
-        self.assertEqual(str(Note("D" ).h_down()),  "C#")
-        self.assertEqual(str(Note("E" ).h_down()),  "D#")
+        self.assertEqual(str(Note("C#"       ).h_down()), "C")
+        self.assertEqual(str(Note("D#"       ).h_down()), "D")
+        self.assertEqual(str(Note("D"        ).h_down()), "C#")
+        self.assertEqual(str(Note("E"        ).h_down()), "D#")
+        self.assertEqual(str(Note("Eb", False).h_down()), "D")
 
 if __name__ == '__main__':
     unittest.main()
